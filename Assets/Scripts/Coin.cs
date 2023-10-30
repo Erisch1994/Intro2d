4f22edc195b83ea68a196ea3835880e802c8c2bd
Erisch1994 <erisch0317@skola.goteborg.se>
2023-10-30 12:45:53 +0100

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public TextManager textManager;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        textManager.AddCoins();
+        Destroy(gameObject);
+    }
+}
